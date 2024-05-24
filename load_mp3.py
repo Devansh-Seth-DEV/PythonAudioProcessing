68aff07be0953b70243d28b63d1d330e01c6cbb8
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2024-05-24 12:03:17 +0530

    Add files via upload

--- load_mp3.py
+++ load_mp3.py
@@ -9,7 +9,7 @@ LoadWAV     = lambda file_path: AudioSegment.from_wav(file_path);
 INC_VOL     = lambda audio, db: audio + db; 
 REP_AUD     = lambda audio, rep: audio * rep;
 FADE_IN_AUD = lambda audio, millisec: audio.fade_in(millisec);
-EXPORT_AUD  = lambda audio, file_path, expFormat: audio.export(file_path, format = expFormat)
+EXPORT_AUD  = lambda audio, file_path, expFormat: audio.export(file_path, format = expFormat);
 
 def PlayAudio(audio_obj: AudioSegment, title: str="audio") -> None:
     try:
@@ -19,20 +19,20 @@ def PlayAudio(audio_obj: AudioSegment, title: str="audio") -> None:
         print(f"Stopping {title} ...\n");
 
 def main() -> None:
-    print("Loading mp3 audio ...")
+    print("Loading mp3 audio ...");
     audio_mp3: AudioSegment   = LoadMP3(AUDIO_FILE);
 
-    audio_mp3: AudioSegment   = INC_VOL(audio_mp3, 6)           # Increases the volume by 6dB
+    audio_mp3: AudioSegment   = INC_VOL(audio_mp3, 6);          # Increases the volume by 6dB
 
-    audio_mp3: AudioSegment   = REP_AUD(audio_mp3, 2)           # Repeate the clip 2 times
+    audio_mp3: AudioSegment   = REP_AUD(audio_mp3, 2);          # Repeate the clip 2 times
 
     audio_mp3: AudioSegment   = FADE_IN_AUD(audio_mp3, 2000);   # Fade in for 2000 milisec
 
-    print("Converting mp3 to wav ...")
+    print("Converting mp3 to wav ...");
     EXPORT_AUD(audio_mp3, OUTPUT_FILE, "wav");
     print("Successfully converted");
 
-    print("Loading exported wav audio ...\n")
+    print("Loading exported wav audio ...\n");
     audio_wav: AudioSegment   = LoadWAV(OUTPUT_FILE);
 
     PlayAudio(audio_mp3, "Vibe ON");
